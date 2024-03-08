535ddd334d50e2e6452f5d88185e3539f05d2e52
AS-Groop <asadbek.sheraliyev07@mail.ru>
2024-03-08 08:27:18 +0300

    fix css bug

--- src/components/view/login/LoginAuth.vue
+++ src/components/view/login/LoginAuth.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen relative flex justify-center items-center">
+  <div class="h-screen overflow-hidden relative flex justify-center items-center">
     <Blob className="w-[400px] h-[400px] top-1/2 right-1/2"/>
     <Blob className="w-[200px] h-[300px] bottom-1/2 left-2/3 spin"/>
     <div class="p-8 relative z-10 max-w-6xl rounded-2xl shadow-[0_0_8px_rgba(0,0,0,0.35)]">
